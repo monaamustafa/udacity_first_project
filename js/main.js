d9db384c73090c859f1d659eb23abb47073a16c0
monaamustafa <monaamustafa7@gmail.com>
2021-07-16 03:08:38 +0200

    fist project

--- js/main.js
+++ js/main.js
@@ -1,30 +1,19 @@
-// dynamic padding top each section 
-window.addEventListener('load', function () {
-    let navbar = document.querySelector('ul');
-    let content = document.querySelectorAll('.mainSection');
-    for (let k = 0; k < content.length; k++) {
-        content[k].style.paddingTop = navbar.clientHeight + "px";
-    }
-})
-//             Add Active Class  
-// let link = document.querySelectorAll('ul li a');
-// for (let i = 0; i < link.length; i++) {
-//     link[i].addEventListener('click', function () {
-//         for (let j = 0; j < link.length; j++) {
-//             link[j].classList.remove('active');
-//         }
-//         link[i].classList.add('active');
-//     });
-// }
-// dynamic scroll
+let navbar = document.querySelector('ul');
 let listItem = document.querySelectorAll('nav ul li');
 let sectionItems = document.querySelectorAll('.mainSection');
+// dynamic padding top each section 
+window.addEventListener('load', function () {
+    for (let k = 0; k < sectionItems.length; k++) {
+        sectionItems[k].style.paddingTop = navbar.clientHeight + "px";
+    }
+})
+// dynamic scroll
 for (let i = 0; i < sectionItems.length; i++) {
     listItem[i].addEventListener('click', () => {
         sectionItems[i].scrollIntoView({
             behavior: 'smooth'
         });
-// remove active class 
+        // remove active class 
         for (let j = 0; j < listItem.length; j++) {
             listItem[j].classList.remove('active');
         }
@@ -35,22 +24,21 @@ for (let i = 0; i < sectionItems.length; i++) {
 
     });
 }
-
 //Scroll-top
 let bodyScroll = document.querySelector('html, body');
 let iconScroll = document.querySelector('.scroll-top');
-
-window.addEventListener('scroll' , function(){
-   if(bodyScroll.scrollTop >= 60){
-      iconScroll.style.display = 'block';
-   }else{
-      iconScroll.style.display = 'none';
-   }
+window.addEventListener('scroll', function () {
+    if (bodyScroll.scrollTop >= 60) {
+        iconScroll.style.display = 'block';
+    } else {
+        iconScroll.style.display = 'none';
+    }
 });
-iconScroll.addEventListener('click',function(){
+iconScroll.addEventListener('click', function () {
     // window.scrollTo(0, 0);
-    window.scrollTo({ top: 0, behavior: 'smooth' });
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
 
 });
-
-
